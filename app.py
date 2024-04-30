4ddc4a54fca9406b117515ce36bfe9c06308139d
Kenneth <quibelkenneth@gmail.com>
2024-04-30 21:46:03 +0800

    initial commit

---
+++ app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+
+# Load the model
+model_path = './model/cnn_model.h5'
+model = load_model(model_path)
+
+# Define class names
+class_names = ['angular leaf spot', 'bean rust', 'healthy']
+
+# Function to make predictions
+def predict(image_file):
+    img = image.load_img(image_file, target_size=(224, 224))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array /= 255.0
+    prediction = model.predict(img_array)
+    predicted_class = np.argmax(prediction)
+    predicted_class_name = class_names[predicted_class]
+    accuracy = np.max(prediction)
+    return predicted_class_name, accuracy
+
+# Streamlit app
+def main():
+    st.title("Bean Leaf Disease Classification")
+
+    # File uploader
+    uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+
+    if uploaded_file is not None:
+        # Display uploaded image
+        image = Image.open(uploaded_file)
+        st.image(image, caption="Uploaded Image", use_column_width=True)
+
+        # Make prediction
+        predicted_class, accuracy = predict(uploaded_file)
+        st.write("Prediction:", predicted_class)
+        st.write("Accuracy:", accuracy)
+
+if __name__ == "__main__":
+    main()
